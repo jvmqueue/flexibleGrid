8f43c54ccb4e6727786cec8c3ab6f019604059ee
jvmqueue <wheels0090@yahoo.com>
2015-05-22 12:17:36 -0400

    Moving getData to utils.js

--- resume/site/scripts/util.js
+++ resume/site/scripts/util.js
@@ -1,7 +1,7 @@
 var mzc = mzc || {};
 mzc.util = (function(w, d, $){
-
- var _fnc = {
+  var _fnc = {
+      that:{},
       setListener:function(options){
         $(options.selector).on(options.event, options.data, options.listener);
       },
@@ -16,7 +16,37 @@ mzc.util = (function(w, d, $){
           }          
         });
     		return json;
-    }
+      }, // End convertXmlToJson
+      getData:function(options){
+        var that = this;
+        var noCache = new Date().getMilliseconds();
+        $.ajax({
+          url:options.path + '?noCache=' + noCache,
+          context:d.body,
+          'text.xml':jQuery.parseXML,
+          crossDomain:false,
+          dataType:'xml',
+          ifModified:true,      
+          success:function(paramData){
+            var nodeName = options.xmlNodeName;
+            // TODO: mzc.util.fnc.convertXmlToJson should be convertXmlToJson, because we are now local
+            var jsonResponse = mzc.util.fnc.convertXmlToJson(paramData, nodeName);
+            _fnc.that['jsonResponse'] = jsonResponse;
+          },
+          statusCode:{
+            404:function(){console.log('Exception: 404 - file not found');}
+          },
+          error:function(paraError){
+            for(var arg in arguments){
+              console.group('ERROR');
+                console.log('arguments:\t', arguments[arg]);
+              console.groupEnd(); 
+            }
+          }
+        }).done(function(){/* anything after done */});
+
+      } // End getData
+
   }; // End _fnc
 
   return{
