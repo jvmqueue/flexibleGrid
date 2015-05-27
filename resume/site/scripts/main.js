24719ddd15271bbf87118deafcf450adaa62544a
jvmqueue <wheels0090@yahoo.com>
2015-05-27 14:05:48 -0400

    adding functionality for education template

--- resume/site/scripts/main.js
+++ resume/site/scripts/main.js
@@ -4,7 +4,9 @@ mzc.main = (function(w, d, $){
 	    defaults:{
 	      PDF:'docs/yourResumeGoesHere.pdf',
 	      WORD:'docs/yourResumeGoesHere.doc',
-	      heading:{}
+	      heading:{},
+	      template:'',
+	      templateEducation:'',
 	  	},
 	   	changeTitle:function(){
 	    	console.log('CHANGE TITLE MODEL LISTENER');
@@ -31,6 +33,27 @@ mzc.main = (function(w, d, $){
 				cache:options.cache
 			});
 
+
+			if(options.fileType == 'text'){
+				w.setTimeout(function(){
+
+					var reg = /(^|\W)id="(\w+)/g;
+					var templateId = reg.exec(data.responseText);
+					var strTemplateId = templateId[2];
+					switch(strTemplateId){
+						case 'templateJumboTron':
+							that.set( 'template', $(data.responseText).html() );
+							break;
+						case 'templateEducation':
+							that.set( 'templateEducation', $(data.responseText).html() );
+							break;
+						default:
+							// TODO: model.getData discovered undefined case
+					}
+
+				}, 777);
+			}
+
 			var interval = w.setInterval(function(){ // wait for data to come back from xmlHttpRequest
 				if(!!data.responseXML){
 					w.clearInterval(interval);
@@ -52,29 +75,63 @@ mzc.main = (function(w, d, $){
 		selectorBtnDownLoadFile:'#modalListContainer',
 		selectorNodeScreenMask:'#screenModalBackground',
 		selectorFormDownLoad:'#frmScreen',
+		template:'',
 		main:function(){
 			this.addListeners();
 			var that = this;
-			this.model.getData( {path:'data/resume.xml', fileType:'xml', xmlNodeName:'title', cache:false} );
+			that.model.getData( {path:'data/resume.xml', fileType:'xml', xmlNodeName:'title', cache:false} );
+			that.model.getData( {path:'data/education.xml', fileType:'xml', xmlNodeName:'title', cache:false} );
+			w.setTimeout(function(){
+				that.model.getData( {path:'templates/titleTemplate.html', fileType:'text', xmlNodeName:'', cache:false} );
+			}, 333);
+			w.setTimeout(function(){
+				that.model.getData( {path:'templates/educationTemplate.html', fileType:'text', xmlNodeName:'', cache:false} );
+			}, 444);			
+
 		},
 		initialize:function(){
 			this.main();
 		},
+		renderJumboTron:function(){
+			var templateHtml = null;
+			var template = null;
+			
+			templateHtml = this.model.get('template');
+			template = _.template(templateHtml);
+			var data = this.model.get('heading');
+			var collection = '';
+			for(var name in data){
+				collection += template(templateVal = data[name]);
+			}
+			this.$el.html(collection);
+			this.containerBtn.removeClass('hide');
+		},
+		renderEducation:function(){
+			var templateHtml = this.model.get('templateEducation');
+
+
+
+		},
 		events:{
 
 		},
+		renderTemplate:function(e){
+			var thisView = this.data; // scoping when we define listener, we send context
+			var templateName = this.templateName;
+
+			switch(templateName){
+				case 'jumbotron':
+					thisView.renderJumboTron();
+					break;
+				case 'education':
+					thisView.renderEducation();
+				default:
+					/* TODO: throw exception discovered name having no associated template */
+			}
+			
+		},
 		render:function render(e){
-			var thisView = this.data; // scoping: sending view instance during listener definition
-			var data = this.data.model.get('heading'); // data from model which converted XML to JSON
- 			var template = _.template( $('#templateTry').html() ); // templates have html as arg
- 			var collection = ''; // used to concatenate templates
 
- 			for(var name in data){ // could use .each, but this is more readable, more efficient
- 				collection += template(anyVal = data[name]);
- 			}
-
- 			thisView.$el.html(collection); // append with our html string
- 			thisView.containerBtn.removeClass('hide');
 		},
 		openDownLoadForm:function(e){
 			// open model
@@ -137,6 +194,9 @@ mzc.main = (function(w, d, $){
 			});
 			/* avoid using generic event, otherwise it will be triggered everytime we affect model data */
 			that.model.on('change:heading', that.render, {data:that});
+
+			that.model.on('change:template', that.renderTemplate, {data:that, templateName:'jumbotron'});
+			that.model.on('change:templateEducation', that.renderTemplate, {data:that, templateName:'education'});
 		}
 	});
 
