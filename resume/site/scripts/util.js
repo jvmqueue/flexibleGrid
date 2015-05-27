909286ddbd41c13875f1768ade0ae91a624e23d0
jvmqueue <wheels0090@yahoo.com>
2015-05-27 14:07:36 -0400

    defined getChildNodes to convert multi-dimensional XML to JSON

--- resume/site/scripts/util.js
+++ resume/site/scripts/util.js
@@ -10,13 +10,44 @@ mzc.util = (function(w, d, $){
         var xmlParent = xml.getElementsByTagName(paramNodeName)[0];
         var json = {};
 
-    		$(xmlParent.childNodes).each(function(index, elm){
+        $(xmlParent.childNodes).each(function(index, elm){
           if(elm.nodeType == 1){          
             json[elm.nodeName] = elm.firstChild.nodeValue;
+            
+            if(elm.childNodes.length > 1){
+              json = _fnc.getChildNodes(elm.childNodes[0]);
+            }
+            
           }          
         });
-    		return json;
+        return json;
       }, // End convertXmlToJson
+      intCounter:0,
+      xmlNodes:{},
+      getChildNodes:function(paramNode){
+        var node = paramNode;
+        var ELEMENT_NODE = 1;
+        var TEXT_NODE = 3;
+        if(!!node.nodeName && node.nodeType == ELEMENT_NODE){
+          _fnc.xmlNodes[node.nodeName] = node.firstChild.nodeValue;
+        }else if(!!node.nodeName && node.nextSibling.nodeType != TEXT_NODE){
+          try{
+            for(var i = 0, len = node.nextSibling.childNodes.length; i < len; i++){
+              if(node.nextSibling.childNodes[i].nodeType == ELEMENT_NODE){
+                _fnc.xmlNodes[node.nextSibling.childNodes[i].nodeName] = node.nextSibling.childNodes[i].firstChild.nodeValue;
+              }
+            }
+          }catch(EX){
+            //continue;
+          }
+        }
+        if(!!node.nextSibling && !!node.childNodes){
+            if(!!node && _fnc.intCounter++ < 3){
+              !!node.nextSibling.childNodes[1] ? _fnc.getChildNodes(node.nextSibling.childNodes[1]) : _fnc.getChildNodes(node.nextSibling); 
+            }
+        }
+        return _fnc.xmlNodes;
+      },
       getData:function(options){
         var noCache = new Date().getMilliseconds();
         return $.ajax({ // return the response so that callee does not have to look for reponse
@@ -48,7 +79,7 @@ mzc.util = (function(w, d, $){
   }; // End _fnc
 
   return{
-  	fnc:_fnc
+    fnc:_fnc
   };
 
 })(window, document, jQuery);
